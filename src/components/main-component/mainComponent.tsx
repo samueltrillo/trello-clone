b61e575054a36bb9003a34a1a1c808548499d238
Fdobboletta <facudobbo@gmail.com>
2021-06-03 12:59:38 -0300

    Added new styles to the modal and a function Close for develop purposes

--- src/components/main-component/mainComponent.tsx
+++ src/components/main-component/mainComponent.tsx
@@ -4,13 +4,19 @@ import styles from './mainComponent.module.css';
 import Columns from './columns';
 import Modal from './modal';
 import { Props } from './index';
+import styled from 'styled-components';
+
+const MainPage = styled.div`
+    display:flex;
+    flex-flow: column;
+`
 
 const MainComponent: React.FC<Props> = ({columns}) => {
 
     const [isOpened, setIsOpened] = useState(false);
-  
+    
     return (
-        <div>
+        <MainPage>
             <div className={styles.mainContainer}>
                 {
                     columns && columns.map(columnsProps => (
@@ -24,15 +30,9 @@ const MainComponent: React.FC<Props> = ({columns}) => {
                     ))
                 }
             </div>
-            <button onClick={() => setIsOpened(true)}>Open Modal</button>
-            {
-                isOpened
-                ?
-                    <Modal />
-                :
-                    undefined
-            }
-        </div>
+            <button onClick={() => setIsOpened(!isOpened)}>Open Modal</button>
+            { isOpened ? <Modal setOpened={setIsOpened}/> : undefined }
+        </MainPage>
     );
 };
 
